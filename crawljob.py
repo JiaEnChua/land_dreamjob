a78c30100e9a4abc422af733b94b34973a03beab
Jia En Chua <chuaj@purdue.edu>
2017-09-23 02:29:33 -0400

    Run browser in background

--- crawljob.py
+++ crawljob.py
@@ -1,9 +1,15 @@
+import time
 from selenium import webdriver
 from bs4 import BeautifulSoup as soup
 
+# utilize PhantomJS to run browser in the background
+driver = webdriver.PhantomJS('C:/Users/Jia En Chua/PycharmProjects/land_dreamjob/phantomjs.exe')
+driver.set_window_size(1920,1080) # set the window size to avoid error
+try:
+    driver.get('https://www.glassdoor.com/')
+except Exception as e:
+    driver.save_screenshot('debugjobscrawler.png') # capture error for debugging
 
-driver = webdriver.Chrome()
-driver.get('https://www.glassdoor.com/')
 driver.find_element_by_name('sc.keyword').send_keys('Software Intern')
 driver.find_element_by_id('LocationSearch').clear()
 driver.find_element_by_id('LocationSearch').send_keys('US')
@@ -16,10 +22,13 @@ driver.quit() # close the website
 
 # collect all the containers that contain different jobs
 containers = page_soup.findAll("li",{"class":"jl"})
+
+# generate spreadsheet
 filename = "job.csv"
 f = open(filename,"w")
 headers = "Title, Company, Location, Salary, Post Date\n"
 f.write(headers)
+
 for container in containers:
     flexbox = container.findAll("div",{"class":"flexbox"})
     title = flexbox[0].div.a.text
